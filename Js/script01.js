5241992dade1548f66edec15fbd5ccd5b767be33
yuriLou12 <44472738+yuriLou12@users.noreply.github.com>
2020-11-18 16:18:11 -0300

    lololo
    lolo

---
+++ Js/script01.js
@@ -0,0 +1,17 @@
+alert('coroi menor olha eu aki em outro arquivo')
+document.getElementById('none').value = 'EAW SHOUNEM'
+//comentário de uma linha
+/*
+comentario em varias linhas
+isso permite o encapsulamento de pensamentos
+*/
+/*
+Tipos de variáveis: String, Number(Int, Float), Boolean(true, false)
+Regras: 
+        1º Não pode ser iniciados com numeros, apenas com letras ou "_"
+        2º Proibido o uso de caractéres especias
+        3º Nada de usar palavras reservadas da linguagem
+Declare as variáveis com a instrução "var"
+O "JS" percebe a diferença de minusculo e maiúsculo
+
+*/
